18c347fe23013000cbb069c868c350474fa84ce0
Will Scullin <whscullin@gmail.com>
2014-01-05 10:33:35 -0800

    Add prodos disk, back out WIP.

--- json/disks/index.js
+++ json/disks/index.js
@@ -3,5 +3,10 @@ disk_index = [
      "filename": "json/disks/dos33master.json",
      "name": "DOS 3.3 Master",
      "category": "System"
+  },
+  {
+     "filename": "json/disks/prodos.json",
+     "name": "ProDOS",
+     "category": "System"
   }
 ];
