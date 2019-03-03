dbc2315552c4ebf372fc2bc7f1c492f570e224f9
Will Scullin <scullin@scullinsteel.com>
2019-03-02 22:46:37 -0800

    Port tools to node.

--- json/disks/index.js
+++ json/disks/index.js
@@ -1,12 +1,12 @@
 disk_index = [
   {
-     "filename": "json/disks/dos33master.json",
-     "name": "DOS 3.3 Master",
-     "category": "System"
+    "filename": "json/disks/dos33master.json",
+    "name": "DOS 3.3 Master",
+    "category": "System"
   },
   {
-     "filename": "json/disks/prodos.json",
-     "name": "ProDOS",
-     "category": "System"
+    "filename": "json/disks/prodos.json",
+    "name": "ProDOS",
+    "category": "System"
   }
 ];
