8ddb17202b21887a965dc225e3dccb2405c69018
Will Scullin <scullin@scullin.com>
2021-02-07 21:54:11 -0800

    fix test typing

--- test/js/formats/util.spec.ts
+++ test/js/formats/util.spec.ts
@@ -3,13 +3,21 @@ import { compareSequences } from './util';
 describe('compareSequences', () => {
     it('matches at pos 0', () => {
         expect(
-            compareSequences([0x01, 0x02, 0x03], [0x01, 0x02, 0x03], 0)
+            compareSequences(
+                new Uint8Array([0x01, 0x02, 0x03]),
+                [0x01, 0x02, 0x03],
+                0
+            )
         ).toBeTruthy();
     });
 
     it('matches at pos 1', () => {
         expect(
-            compareSequences([0x00, 0x01, 0x02, 0x03], [0x01, 0x02, 0x03], 1)
+            compareSequences(
+                new Uint8Array([0x00, 0x01, 0x02, 0x03]),
+                [0x01, 0x02, 0x03],
+                1
+            )
         ).toBeTruthy();
     });
 });
