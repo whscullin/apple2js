1802f79769987777f710566732c4529c6aeb6df9
Will Scullin <scullin@scullin.com>
2021-02-23 19:23:25 -0800

    Add audit disk

--- json/disks/index.js
+++ json/disks/index.js
@@ -1,5 +1,10 @@
 disk_index = [
   {
+    "filename": "json/disks/audit.json",
+    "name": "Apple II Audio",
+    "category": "Utility"
+  },
+  {
     "filename": "json/disks/dos33master.json",
     "name": "DOS 3.3 Master",
     "category": "System"
