05b2627b048299eb9c630d4cf7016a354284069c
Ian Flanigan <flan@google.com>
2021-03-27 16:22:44 +0100

    Typescriptify all of the UI peripherals
    This converts the audio, gamepad, keyboard, printer, and tape
    peripherals into Typescript. This is a pretty mechanical change.
    
    It does fix issue #72, though.

---
+++ js/ui/printer.ts
@@ -0,0 +1,92 @@
+/* Copyright 2010-2019 Will Scullin <scullin@scullinsteel.com>
+ *
+ * Permission to use, copy, modify, distribute, and sell this software and its
+ * documentation for any purpose is hereby granted without fee, provided that
+ * the above copyright notice appear in all copies and that both that
+ * copyright notice and this permission notice appear in supporting
+ * documentation.  No representations are made about the suitability of this
+ * software for any purpose.  It is provided "as is" without express or
+ * implied warranty.
+ */
+
+import { byte } from '../types';
+
+/**
+  * Printer UI. The "paper" is bound to the element selected by the input.
+  *
+  * Every line that is output to the printer is added as a <div> to the paper.
+  * The high bit of all characters is stripped and only visible characters are
+  * added to the output. The following characters receive special treatment:
+  *
+  * *   `EOT` (ASCII 4): deletes last character
+  * *   `HT` (ASCII 9): replaced with 8 spaces
+  * *   `LF` (ASCII 10): silently removed
+  * *   `CR` (ASCII 13): a newline and carriage return
+  */
+export default class Printer {
+    private paper: HTMLElement;
+    private _lineBuffer = '';
+    private _line: HTMLElement;
+    private _rawLen = 0;
+    private _raw = new Uint8Array(1024);
+
+    /**
+     * Creates a new printer bound to the given element.
+     * @param {string} el The selector of the element on which to bind the "paper".
+     */
+    constructor(el: string) {
+        this.paper = document.querySelector(el)!;
+        this.newLine();
+    }
+
+    private newLine() {
+        this._line = document.createElement('div');
+        this._line.classList.add('line');
+        this._line.innerText = this._lineBuffer;
+        this.paper.append(this._line);
+        this._lineBuffer = '';
+    }
+
+    putChar(val: byte) {
+        const ascii = val & 0x7f;
+        const visible = val >= 0x20;
+        const c = String.fromCharCode(ascii);
+
+        if (c === '\r') {
+            this.newLine();
+        } else if (c === '\n') {
+            // eat for now
+        } else if (c === '\t') {
+            // possibly not right due to tab stops
+            this._lineBuffer += '        ';
+        } else if (ascii === 0x04) {
+            this._lineBuffer = this._lineBuffer.slice(0, -1);
+        } else if (visible) {
+            this._lineBuffer += c;
+        }
+        this._line.innerText = this._lineBuffer;
+        this._raw[this._rawLen] = val;
+        this._rawLen++;
+        if (this._rawLen > this._raw.length) {
+            const newRaw = new Uint8Array(this._raw.length * 2);
+            newRaw.set(this._raw);
+            this._raw = newRaw;
+        }
+    }
+
+    clear() {
+        this._lineBuffer = '';
+        this.paper.innerHTML = '';
+        this.newLine();
+        this._raw = new Uint8Array(1024);
+        this._rawLen = 0;
+    }
+
+    hasPrintout() {
+        return this.paper.innerText.length > 0;
+    }
+
+    getRawOutput() {
+        return this._raw.slice(0, this._rawLen);
+    }
+}
---
+++ js/ui/tape.ts
@@ -0,0 +1,85 @@
+
+/* Copyright 2010-2019 Will Scullin <scullin@scullinsteel.com>
+ *
+ * Permission to use, copy, modify, distribute, and sell this software and its
+ * documentation for any purpose is hereby granted without fee, provided that
+ * the above copyright notice appear in all copies and that both that
+ * copyright notice and this permission notice appear in supporting
+ * documentation.  No representations are made about the suitability of this
+ * software for any purpose.  It is provided "as is" without express or
+ * implied warranty.
+ */
+
+import { TapeData } from '../types';
+import Apple2IO from '../apple2io';
+import { debug } from '../util';
+
+export const TAPE_TYPES = ['wav', 'aiff', 'aif', 'mp3', 'm4a'] as const;
+
+export default class Tape {
+    constructor(private readonly io: Apple2IO) {}
+
+    public doLoadLocalTape(file: File, done: () => void) {
+        const kHz = this.io.getKHz();
+
+        // Audio Buffer Source
+        let context: AudioContext;
+        if (AudioContext) {
+            context = new AudioContext();
+        } else {
+            window.alert('Not supported by your browser');
+            done();
+            return;
+        }
+
+        const fileReader = new FileReader();
+        fileReader.onload = (ev: ProgressEvent) => {
+            const target: FileReader = ev.target as FileReader;
+            const result: ArrayBuffer = target.result as ArrayBuffer;
+            context.decodeAudioData(result, (buffer) => {
+                const buf: TapeData = [];
+                const data = buffer.getChannelData(0);
+                let datum = data[0];
+                let old = (datum > 0.0), current;
+                let last = 0;
+                let delta: number;
+                debug('Sample Count: ' + data.length);
+                debug('Sample rate: ' + buffer.sampleRate);
+                for (let idx = 1; idx < data.length; idx++) {
+                    datum = data[idx];
+                    if ((datum > 0.1) || (datum < -0.1)) {
+                        current = (datum > 0.0);
+                        if (current != old) {
+                            delta = idx - last;
+                            if (delta > 2000000) {
+                                delta = 2000000;
+                            }
+                            let ival = delta / buffer.sampleRate * 1000;
+                            if (ival >= 0.550 && ival < 0.750) {
+                                ival = 0.650; // Header
+                            } else if (ival >= 0.175 && ival < 0.225) {
+                                ival = 0.200; // sync 1
+                            } else if (ival >= 0.225 && ival < 0.275) {
+                                ival = 0.250; // 0 / sync 2
+                            } else if (ival >= 0.450 && ival < 0.550) {
+                                ival = 0.500; // 1
+                            } else {
+                                // debug(idx + ' ' + buf.length + ' ' + ival);
+                            }
+                            buf.push([ival * kHz, current]);
+                            old = current;
+                            last = idx;
+                        }
+                    }
+                }
+                this.io.setTape(buf);
+                if (done) {
+                    done();
+                }
+            }, function (error) {
+                window.alert(error.message);
+            });
+        };
+        fileReader.readAsArrayBuffer(file);
+    }
+}
