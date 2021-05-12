dadf1a7fc731009e3db85d6f338174bc568bb451
Will Scullin <scullin@scullin.com>
2021-05-11 20:59:43 -0700

    canvas tests

---
+++ test/js/__mocks__/apple2shader.js
@@ -0,0 +1,10 @@
+export const screenEmu = (function () {
+    return {
+        DisplayConfiguration: class {},
+        Point: class {},
+        ScreenView: class {
+            initOpenGL() {}
+        },
+        Size: class{},
+    };
+})();
