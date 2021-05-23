649713ff4bb472d49c06600de63bf216607a0395
Will Scullin <scullin@scullin.com>
2021-05-23 14:22:56 -0700

    fix tests

--- test/js/__mocks__/apple2shader.js
+++ test/js/__mocks__/apple2shader.js
@@ -1,5 +1,13 @@
 export const screenEmu = (function () {
     return {
+        C: {
+            NTSC_DETAILS: {
+                imageSize: {
+                    width: 560,
+                    height: 192,
+                },
+            },
+        },
         DisplayConfiguration: class {},
         Point: class {},
         ScreenView: class {
