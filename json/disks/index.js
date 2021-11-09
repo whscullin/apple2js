ddc0e7efb6a0ee90874893b8f2be7e8e133cea8d
Joe Auman <brain.recall@gmail.com>
2021-11-08 19:15:22 -0500

    added offical version of Oregon Trail

--- json/disks/index.js
+++ json/disks/index.js
@@ -966,8 +966,17 @@ disk_index = [
     "category": "Game"
   },
   {
+    "filename": "json/disks/Oregon_Trail_Disk_1_of_2.json",
+    "name": "Oregon Trail Disk 1",
+    "category": "Games"
+  },
+  {
+    "filename": "json/disks/Oregon_Trail_Disk_2_of_2.json",
+    "name": "Oregon Trail Disk 2",
+    "category": "Games"
+  },
+  {
     "filename": "json/disks/beagle_graphics.json",
-    "e": true,
     "name": "Beagle Graphics",
     "category": "Graphics"
   },
