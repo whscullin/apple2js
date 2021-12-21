70ec626dd01b69a7db62d70afdff4e5af81690f7
Will Scullin <scullin@scullinsteel.com>
2021-12-21 12:35:26 -0800

    Clean up copyright notices (#93)

--- js/util.ts
+++ js/util.ts
@@ -1,14 +1,3 @@
-/* Copyright 2010-2019 Will Scullin <scullin@scullinsteel.com>
- *
- * Permission to use, copy, modify, distribute, and sell this software and its
- * documentation for any purpose is hereby granted without fee, provided that
- * the above copyright notice appear in all copies and that both that
- * copyright notice and this permission notice appear in supporting
- * documentation.  No representations are made about the suitability of this
- * software for any purpose.  It is provided ' as is'  without express or
- * implied warranty.
- */
-
 import { byte, memory, word } from './types';
 
 /*eslint no-console: 0*/
