ff58a6f7b3fd4b0bcd822c443b562f9f063bae86
Will Scullin <scullin@scullin.com>
2022-05-12 18:55:31 -0700

    Add touch screen handling

--- js/ui/mouse.ts
+++ js/ui/mouse.ts
@@ -1,30 +1,84 @@
 import type Mouse from '../cards/mouse';
 import { enableMouseMode } from './joystick';
 
+type TouchEventTarget = TouchEvent & { target: HTMLCanvasElement };
+
 export class MouseUI {
     private mouse: Mouse;
 
     constructor(private canvas: HTMLCanvasElement)  {
-        this.canvas.addEventListener(
-            'mousemove',
-            (event: MouseEvent & { target: HTMLCanvasElement} ) => {
-                const { offsetX, offsetY, target } = event;
-                this.mouse.setMouseXY(
-                    offsetX,
-                    offsetY,
-                    target.clientWidth,
-                    target.clientHeight
-                );
+        const updateTouchXY = (event: TouchEventTarget) => {
+            const { targetTouches, target } = event;
+            if (targetTouches.length < 1) {
+                return;
             }
-        );
+            const rect = target.getBoundingClientRect();
+            const leftPad = (rect.width - 560) / 2 + rect.left;
+            const topPad = (rect.height - 384) / 2 + rect.top;
+            const { clientX, clientY } = targetTouches[0];
+            const xPos = clientX - leftPad;
+            const yPos = clientY - topPad;
+            this.mouse.setMouseXY(
+                Math.max(Math.min(xPos, 559), 0),
+                Math.max(Math.min(yPos, 383), 0),
+                560,
+                384,
+            );
+        };
 
-        this.canvas.addEventListener('mousedown', () => {
-            this.mouse.setMouseDown(true);
-        });
+        if ('ontouchstart' in window) {
+            this.canvas.addEventListener(
+                'touchmove',
+                (event: TouchEventTarget) => {
+                    updateTouchXY(event);
+                }
+            );
 
-        this.canvas.addEventListener('mouseup', () => {
-            this.mouse.setMouseDown(false);
-        });
+            this.canvas.addEventListener(
+                'touchstart',
+                (event: TouchEventTarget) => {
+                    updateTouchXY(event);
+                    setTimeout(() => this.mouse.setMouseDown(true), 100);
+                }
+            );
+
+            this.canvas.addEventListener(
+                'touchend',
+                (event: TouchEventTarget) => {
+                    updateTouchXY(event);
+                    this.mouse.setMouseDown(false);
+                }
+            );
+
+            this.canvas.addEventListener(
+                'touchcancel',
+                (event: TouchEventTarget) => {
+                    updateTouchXY(event);
+                    this.mouse.setMouseDown(false);
+                }
+            );
+        } else {
+            this.canvas.addEventListener(
+                'mousemove',
+                (event: MouseEvent & { target: HTMLCanvasElement} ) => {
+                    const { offsetX, offsetY, target } = event;
+                    this.mouse.setMouseXY(
+                        offsetX,
+                        offsetY,
+                        target.clientWidth,
+                        target.clientHeight
+                    );
+                }
+            );
+
+            this.canvas.addEventListener('mousedown', () => {
+                this.mouse.setMouseDown(true);
+            });
+
+            this.canvas.addEventListener('mouseup', () => {
+                this.mouse.setMouseDown(false);
+            });
+        }
     }
 
     setMouse = (mouse: Mouse) => {
