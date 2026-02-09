759450979a53c99646145eab0f5eefaf6f57ba82
Will Scullin <scullin@scullinsteel.com>
2026-02-08 18:23:06 -0800

    fix: style imports

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,7 @@ const baseConfig = {
                             import: false,
                             modules: {
                                 localIdentName: '[path][name]__[local]',
+                                namedExport: false,
                             },
                         },
                     },
@@ -46,6 +47,7 @@ const baseConfig = {
                             import: false,
                             modules: {
                                 localIdentName: '[path][name]__[local]',
+                                namedExport: false,
                             },
                         },
                     },
